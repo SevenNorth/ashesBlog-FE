b33f0f1d7871cb5fb15fcf63ca53e23c94c8dbf8
冯箭飞 <wb_fengjianfei@meituan.com>
2021-01-12 22:13:09 +0800

    提组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-import ListItem from '../../components/listItem'
+import ListItem from '../../components/list'
 
 const Home :React.FunctionComponent = () =>{
   return (
     <div>
-      <ListItem title={'test'} summary={'123123123'} tags={[1,2,3]}/>
+      <ListItem title={'test'} summary={'123123123'} tags={[1,2,3]}>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </ListItem>
     </div>
   )
 }
